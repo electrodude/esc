0fefabf4fd546a1dffabfcf751b8a2ef5f0df0ae
David Zemon <david@zemon.name>
2015-03-08 22:38:19 -0500

    Fix incorrect module path for spinc

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ Settings settings;
 int main(int argc, char* argv[])
 {
 
-	CompilerModule* mod = ModuleLoader::load("./modules/libspinc.so");
+	CompilerModule* mod = ModuleLoader::load("./modules/spinc/libspinc.so");
 
 	Compiler c;
 
